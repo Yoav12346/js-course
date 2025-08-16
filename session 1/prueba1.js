cede59528fc0cd89d996d6f545341a207ff66ca3
Yoav12346 <yoavllanosculquiton57@gmail.com>
2025-08-16 10:57:23 -0500

    Pimera clase, apredimos variables y como hacer un si eres mayor de edad

---
+++ session 1/prueba1.js
@@ -0,0 +1,7 @@
+let nombre = "Yoav"
+let edad = 18
+let esMayordeEdad = edad >= 18
+
+console.log("Nombre:", nombre)
+console.log("Edad:", edad)
+console.log("Es mayor de edad:", esMayordeEdad)
--- session 1/session-1.js
+++ session 1/session-1.js
@@ -1,1 +1,42 @@
+// este en un mnesaje de consola 
+
 console.log("hola mundo")
+
+// Variable y Constantessss
+let edad= 18 // Variable que puede cambiar
+const nombre = "Juan" // Constante que no puede cambiar
+// Palabra reservada 
+var ciudad = "Santiago"
+
+console.log("edad" , edad)
+console.log("nombre", nombre)
+console.log("ciudad", ciudad)
+
+// Tipos de datos
+
+let apellido = "Llanos" //tipo de dato string
+let ismayor = false // true //tipo de dato booleano
+let pi = 3.14159// tipo de dato number
+
+//concatenaciones 
+console.log("Hola, mi nombre es ${nombre} ${apellido} y tengp ${edad} años.")
+
+//operaciones basicas 
+
+// sumar
+
+let a = 5
+let b = 10
+let suma = a + b
+console.log("suma" , suma)
+
+// restar
+let resta = a - b
+console.log("resta", resta)
+
+// multiplicar
+let multiplicacion = a * b
+console.log("multiplicacion", multiplicacion)
+// dividir
+let division = b / a
+console.log("division", division)
